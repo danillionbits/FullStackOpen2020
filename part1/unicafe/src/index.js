e2e85ef3c0acfe17798e13d602aee56aaf2a17c7
unknown <datpt98@gmail.com>
2020-10-16 21:46:32 +0300

    1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom'
 const Statistics = ({ good, neutral, bad }) => {
   let total = good + neutral + bad
 
-  return(
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
     <div>
-      <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
@@ -29,6 +32,7 @@ const App = () => {
       <button onClick={() => setGood(good+1)}> good </button>
       <button onClick={() => setNeutral(neutral+1)}> neutral </button>
       <button onClick={() => setBad(bad+1)}> bad </button>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
