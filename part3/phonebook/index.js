0cf24eb8f05a4e54764a0456cc1b1892d87521b7
unknown <datpt98@gmail.com>
2020-10-18 18:40:31 +0300

    3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -93,7 +95,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
