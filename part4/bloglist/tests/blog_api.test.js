d8867e968d1e4bbbf6d93eccce503de90abc84fc
unknown <datpt98@gmail.com>
2020-10-28 15:15:42 +0200

    4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,28 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('../tests/test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('blogs length is returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
