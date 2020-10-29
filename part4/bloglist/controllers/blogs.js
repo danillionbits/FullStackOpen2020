796b610d1869f72740384ace968743e145a4ad1f
unknown <datpt98@gmail.com>
2020-10-29 16:21:52 +0200

    4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -6,11 +7,24 @@ blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user')
   res.json(blogs.map(blog => blog.toJSON()))
 })
+
+const getTokenFrom = req => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
   
 blogsRouter.post('/', async (req, res, next) => {
   const body = req.body
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(req)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
