dbbece235942b5330279aa2fee526ee6dc66447d
unknown <datpt98@gmail.com>
2020-10-29 16:31:08 +0200

    4.20

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -8,6 +8,15 @@ const requestLogger = (req, res, next) => {
   next()
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  req.token = null
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    req.token = authorization.substring(7)
+  }
+  next()
+}
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
@@ -16,16 +25,27 @@ const errorHandler = (error, req, res, next) => {
   logger.error(error.message)
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).send({ 
+      error: 'malformatted id' 
+    })
   } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message })
+    return res.status(400).json({ 
+      error: error.message 
+    })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
+
+  logger.error(error.message)
 
   next(error)
 }
 
 module.exports = {
   requestLogger,
+  tokenExtractor,
   unknownEndpoint,
   errorHandler
 }
