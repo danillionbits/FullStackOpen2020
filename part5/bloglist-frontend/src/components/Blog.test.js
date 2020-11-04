53cf3553a5815df84a517f66ee60d6141791dd3e
unknown <datpt98@gmail.com>
2020-11-04 15:48:32 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
 test('renders content', () => {
   const blog = {
-    author: 'blog',
-    title: 'blog',
-    user: 'blog',
-    url: 'blog',
+    author: "blog",
+    title: "blog",
+    user: "blog",
+    url: "www.blog.com",
     likes: 3
 }
 
@@ -18,4 +19,32 @@ test('renders content', () => {
 
   expect(component.container).toHaveTextContent(blog.title)
   expect(component.container).toHaveTextContent(blog.author)
+  expect(component.container).not.toHaveTextContent(blog.url)
+  expect(component.container).not.toHaveTextContent(3)
 })  
+
+test('clicking show button', () => {
+  const user = { 
+    username:"root",
+    id: "5f9aaa8c4b84bb1ec0612a2c",
+    name: "Superuser"
+  }
+  
+  const blog = {
+    author: "tester",
+    title: "testtitle ",
+    user: user,
+    url: "www.test.com",
+    likes: 3
+  }
+
+  const component = render(
+    <Blog blog={blog} user={user} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(blog.url)
+  expect(component.container).toHaveTextContent(3)
+})
